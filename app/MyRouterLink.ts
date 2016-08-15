1855ded439f0d33ee5263cdd2cb927e64c6176f9
Longfld <Longfld@users.noreply.github.com>
2016-08-15 10:12:41 +1000

    Create MyRouterLink.ts

---
+++ app/MyRouterLink.ts
@@ -0,0 +1,49 @@
+
+import {Component, ViewContainerRef, ComponentResolver,
+  ComponentRef, ComponentFactory, ViewChild, ComponentMetadata} from '@angular/core';
+import { ROUTER_DIRECTIVES} from '@angular/router';
+import {DataServices} from './DataServices';
+
+@Component({
+  selector: 'my-router-link',
+  template: '<div #mymenu></div>',
+  directives: [ROUTER_DIRECTIVES],
+  providers: [DataServices]
+})
+
+export class MyRouterLink {
+  @ViewChild('mymenu', { read: ViewContainerRef }) target: ViewContainerRef;
+  private cmpRef: ComponentRef<any>;
+
+  constructor(private dataServices: DataServices, private resolver: ComponentResolver, private _viewContainerRef: ViewContainerRef) { }
+
+  ngAfterViewInit() {
+    if (this.cmpRef) {
+      this.cmpRef.destroy();
+    }
+    this.dataServices.GetMenuLinks()
+      .subscribe(returnedmenu =>
+        (
+          (this.compileToComponent(returnedmenu)).then((factory: ComponentFactory<any>) => {
+            this.cmpRef = this.target.createComponent(factory)
+          })
+        ),
+      error => console.log(<any>error)
+      );
+  }
+
+  ngOnDestroy() {
+    if (this.cmpRef) {
+      this.cmpRef.destroy();
+    }
+  }
+
+  compileToComponent(template1: string): Promise<ComponentFactory<any>> {
+    const metadata = new ComponentMetadata({
+      template: template1,
+      directives: ROUTER_DIRECTIVES
+    });
+    let decoratedCmp = Component(metadata)(class DynamicComponent { });
+    return this.resolver.resolveComponent(decoratedCmp);
+  }
+}
