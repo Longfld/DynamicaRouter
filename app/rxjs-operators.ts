62ffd70573d6640e58ac4c5b822af4473e5e18c9
Longfld <Longfld@users.noreply.github.com>
2016-08-15 10:13:16 +1000

    Create rxjs-operators.ts

---
+++ app/rxjs-operators.ts
@@ -0,0 +1,8 @@
+// Statics
+import 'rxjs/add/observable/throw';
+
+// Operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
