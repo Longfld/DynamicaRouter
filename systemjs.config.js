78bea072eda10338521aef4c1eab1b993de5084d
Longfld <Longfld@users.noreply.github.com>
2017-11-30 16:49:46 +1100

    Angular material 5

--- systemjs.config.js
+++ systemjs.config.js
@@ -14,28 +14,37 @@
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      // angular testing umd bundles
-      '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
-      '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
-      '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
-      '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
-      '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
-      '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
-      '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
-      '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
+      '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
+      '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
+      '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
 
-      '@angular/animations': 'node_modules/@angular/animations/bundles/animations.umd.min.js',
-      '@angular/animations/browser':'node_modules/@angular/animations/bundles/animations-browser.umd.js',
-      '@angular/platform-browser/animations': 'node_modules/@angular/platform-browser/bundles/platform-browser-animations.umd.js',
-
-      '@angular/material': 'npm:@angular/material/material.umd.js',
+      //material
+      '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
+      '@angular/cdk': 'npm:@angular/cdk/bundles/cdk.umd.js',
+      '@angular/cdk/a11y': 'npm:@angular/cdk/bundles/cdk-a11y.umd.js',
+      '@angular/cdk/bidi': 'npm:@angular/cdk/bundles/cdk-bidi.umd.js',
+      '@angular/cdk/coercion': 'npm:@angular/cdk/bundles/cdk-coercion.umd.js',
+      '@angular/cdk/collections': 'npm:@angular/cdk/bundles/cdk-collections.umd.js',
+      '@angular/cdk/keycodes': 'npm:@angular/cdk/bundles/cdk-keycodes.umd.js',
+      '@angular/cdk/observers': 'npm:@angular/cdk/bundles/cdk-observers.umd.js',
+      '@angular/cdk/overlay': 'npm:@angular/cdk/bundles/cdk-overlay.umd.js',
+      '@angular/cdk/platform': 'npm:@angular/cdk/bundles/cdk-platform.umd.js',
+      '@angular/cdk/portal': 'npm:@angular/cdk/bundles/cdk-portal.umd.js',
+      '@angular/cdk/accordion': 'npm:@angular/cdk/bundles/cdk-accordion.umd.js',
+      '@angular/cdk/layout': 'npm:@angular/cdk/bundles/cdk-layout.umd.js',
+      '@angular/cdk/scrolling': 'npm:@angular/cdk/bundles/cdk-scrolling.umd.js',
+      '@angular/cdk/table': 'npm:@angular/cdk/bundles/cdk-table.umd.js',
+      '@angular/cdk/stepper': 'npm:@angular/cdk/bundles/cdk-stepper.umd.js',
 
       // other libraries
-      'rxjs':                       'npm:rxjs'
-      
+      'rxjs': 'npm:rxjs',
+      'hammerjs': 'npm:hammerjs',
+      'tslib': 'npm:tslib/tslib.js'
+
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -46,10 +55,14 @@
       rxjs: {
         defaultExtension: 'js'
       },
-       hammerjs:  { 
-                main: './hammer.js', 
-                defaultExtension: 'js'
-              }
+      "rxjs/operators": {
+        "main": "index.js",
+        "defaultExtension": "js"
+      },
+      hammerjs: {
+        main: './hammer.js',
+        defaultExtension: 'js'
+      }
     }
   });
 })(this);
