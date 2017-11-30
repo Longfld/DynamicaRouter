9849b6e9ac11782ede1206a49e928764c52b4d4a
Longfld <Longfld@users.noreply.github.com>
2017-11-30 16:53:12 +1100

    Angular material 5

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule, JsonpModule } from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import { MyMaterialModule} from './my.material.module'
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
 import { MyRouterLink} from './MyRouterLink';
 
 
 @NgModule({
-  imports:      [ BrowserModule,BrowserAnimationsModule,HttpModule,JsonpModule, routing ],
+  imports:      [ BrowserModule,BrowserAnimationsModule,HttpModule,JsonpModule, MyMaterialModule, routing ],
   declarations: [ AppComponent,MyRouterLink ],
   bootstrap:    [ AppComponent ]
 })
