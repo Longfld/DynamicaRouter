908c88ff541590f016662ba7c06efb0e8daeb9b9
Longfld <Longfld@users.noreply.github.com>
2018-07-05 11:40:06 +1000

    Angular 6

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-import 'rxjs/Rx';
+//import 'rxjs/Rx';
 import 'hammerjs/hammer';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
